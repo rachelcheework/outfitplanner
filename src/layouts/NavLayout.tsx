0a9cbd5333ec10ee5626d7b313147b86c93e3813
Rachel <rachelcheework@gmail.com>
2026-08-12 20:20:11 +0800

    fixed typo

--- src/layouts/NavLayout.tsx
+++ src/layouts/NavLayout.tsx
@@ -6,7 +6,7 @@ const NavLayout = () => {
     <div className="flex flex-col-reverse md:h-screen md:flex-col">
       <Navbar />
 
-      <main className="flex-1 min-h-0 p-6 over bg-green-500">
+      <main className="flex-1 min-h-0 p-6 bg-green-500">
         <Outlet />
       </main>
     </div>
